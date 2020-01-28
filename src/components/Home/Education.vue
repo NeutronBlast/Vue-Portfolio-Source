1a7ae923f52a36713d83fe0e8dae4264fe50e9a0
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2020-01-28 02:41:07 -0400

    fixed text size in timeline
    remember: work summary in cards

--- src/components/Home/Education.vue
+++ src/components/Home/Education.vue
@@ -3,37 +3,47 @@
     <vue-timeline-update
     :date="new Date('2020-01-18')"
     title="UCAB"
-    description="Reached 7th semester of Computer Engineering major in Universidad Católica Andrés Bello"
+    description=""
     thumbnail="https://vida.ucab.edu.ve/wp-content/uploads/sites/2/2017/03/Isologotipo-VidaUCAB-Horizontal-1.png"
     category="COLLEGE"
     icon="school"
     color="black"
-    theme="light"
-  />
+    theme="light">
+    </vue-timeline-update>
+      <p class="timeline-text">
+        Reached 7th semester of Computer Engineering major in Universidad Católica Andrés Bello
+      </p>
+
+
   <!-- Latest update -->
   <vue-timeline-update
     :date="new Date('2019-08-31')"
     title="Cisco CCNA"
-    description="Completed the first module of Cisco CCNA Networking Course"
+    description=""
     thumbnail="https://i.imgur.com/Xaq1YGF.png"
     category="course"
     icon="computer"
     color="blue"
     theme="light">
   </vue-timeline-update>
-
+  <p class="timeline-text">
+    Completed the first module of Cisco CCNA Networking Course
+  </p>
     <!-- Yet another update -->
   <vue-timeline-update
     :date="new Date('2012-09-30')"
     title="Fyr Lois"
-    description="Completed my English course"
+    description=""
     thumbnail="https://http2.mlstatic.com/libros-interchange-4ta-edition-para-fyr-lois-digital-1bono-D_NQ_NP_549811-MLV20675776756_042016-F.jpg"
     category="course"
     icon="textsms"
     color="black"
     theme="light"
-    is-last
-  />
+    is-last>
+  </vue-timeline-update>
+    <p class="timeline-text">
+    Completed my English course
+  </p>
 </div>
 </template>
 
