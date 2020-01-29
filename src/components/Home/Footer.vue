9fd4a5ada65bbfd16023a080000588920419a706
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2020-01-28 22:43:33 -0400

    footer

---
+++ src/components/Home/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <!-- Footer -->
+    <footer class="page-footer font-small blue">
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="/"> Frank Hesse</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <About></About>
     <Gallery></Gallery>
     <Contact></Contact>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Slide from '@/components/Home/Slide.vue'
 import About from '@/components/Home/About.vue'
 import Gallery from '@/components/Home/WorkGallery.vue'
 import Contact from '@/components/Home/Contact.vue'
+import MyFooter from '@/components/Home/Footer.vue'
 
 export default {
   name: 'home',
@@ -20,6 +22,7 @@ export default {
     About,
     Gallery,
     Contact,
+    MyFooter,
   }
 }
 </script>
